3935a44ae85413367344df3920f29515a85af154
Hiranya <agarwh@pedarecc.sa.edu.au>
2022-01-24 20:34:11 +1030

    mvp finished

--- app.py
+++ app.py
@@ -42,9 +42,99 @@ def new_items():
 
     return render_template("itemInput.html")
 
-@app.route("/test")
-def test():
-    return render_template("itemInput.html")
+@app.route("/item/delete")
+def delete():
+    connection = sqlite3.connect(config.DB_FILE)
+    cursor = connection.cursor()
+    id1 =request.args.get('id')
+    print(id)
+    cursor.execute("""DELETE FROM items WHERE id=(?)""",(id1))
+
+
+    connection.commit()
+    cursor.close()
+
+    return render_template("item_list.html")
+
+
+
+
+@app.route("/users")
+def users():
+    connection=sqlite3.connect(config.DB_FILE)
+    connection.row_factory=sqlite3.Row
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    rows=cursor.fetchall()
+    return render_template("user_list.html", rows=rows)
+
+@app.route("/new_user", methods=['GET', 'POST'])
+def new_user():
+    if request.method == 'POST':
+        # firstname1.users = request.form['firstname']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+        yearlevel = request.form['yearlevel']
+        team = request.form['team']
+        connection = sqlite3.connect(config.DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("""INSERT INTO users (firstname,lastname,email,yearlevel,team )VALUES(?,?,?,?,?)""",
+                       (firstname, lastname, email, yearlevel, team))
+        connection.commit()
+
+
+    else:
+        print("")
+
+    return render_template("userInput.html")
+
+@app.route("/borrow", methods=['GET', 'POST'])
+def borrow():
+    if request.method == 'POST':
+
+        item_barcode = request.form['item_barcode']
+        student_id = request.form['student_id']
+        date = request.form['date']
+        connection = sqlite3.connect(config.DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("""INSERT INTO loans (item_id,user_id,date, returned )VALUES(?,?,?,'n')""",
+                       (item_barcode, student_id, date))
+        connection.commit()
+
+
+
+    else:
+        print("")
+
+    return render_template("borrow.html")
+
+@app.route("/returns", methods=['GET', 'POST'])
+def returns():
+    if request.method == 'POST':
+        print("check")
+        item_barcode = request.form['item_barcode']
+        connection = sqlite3.connect(config.DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute('''UPDATE loans SET returned = ? WHERE item_id = ?''', ('y', item_barcode))
+        connection.commit()
+
+    else:
+        print("")
+
+    return render_template("return.html")
+
+@app.route("/loans")
+def loans():
+    connection=sqlite3.connect(config.DB_FILE)
+    connection.row_factory=sqlite3.Row
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM loans")
+    rows=cursor.fetchall()
+    return render_template("loan_list.html", rows=rows)
+
+
+
 
 
 
--- create_db.py
+++ create_db.py
@@ -9,7 +9,7 @@ c.execute("""CREATE TABLE IF NOT EXISTS items(
            name TEXT NOT NULL,
            barcode TEXT NOT NULL, 
            description TEXT NOT NULL,
-           sku TEXT NOT NULL UNIQUE,
+           sku TEXT NOT NULL,
            exgst TEXT NOT NULL
            )""")
 
@@ -18,6 +18,7 @@ c.execute("""CREATE TABLE IF NOT EXISTS loans(
             item_id INTEGER NOT NULL,
             user_id INTEGER NOT NULL,
             date DATE NOT NULL,
+            returned TEXT NOT NULL,
             
             FOREIGN KEY(item_id) REFERENCES item(id)
             FOREIGN KEY(user_id) REFERENCES user(id)
@@ -29,7 +30,17 @@ c.execute("""CREATE TABLE IF NOT EXISTS users(
           firstname TEXT NOT NULL,
           lastname TEXT NOT NULL,
           email TEXT NOT NULL,
-          yearlevel INTEGER NOT NULL
+          yearlevel INTEGER NOT NULL,
+          team TEXT
+          )""")
+
+c.execute("""CREATE TABLE IF NOT EXISTS inventory(
+          id INTEGER PRIMARY KEY,
+          item_name TEXT NOT NULL,
+          total_qty INTEGER NOT NULL,
+          available_qty INTEGER NOT NULL,
+          qty_loaned INTEGER NOT NULL,
+          qty_broken INTEGER TEXT,
           )""")
 
 
